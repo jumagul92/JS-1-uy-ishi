d6be4a54e8a378cb440b2cab02336137c1fc2f20
Jumagul <umarovajumagul76@gmail.com>
2023-07-03 06:45:53 +0100

    create html

---
+++ script.js
@@ -0,0 +1,45 @@
+var name = prompt("Ismingizni kiriting")
+console.log("Mening ismim " + name);
+var age = +prompt("Yoshingizni kiriting")
+console.log("Men " + age + " yoshdaman");
+var task1 = +prompt("Misolni yeching 7 + 4 = ?")
+console.log("1-misol: 7 + 4 = " + (7+4) + " Sizning javobingiz " + task1);
+var task2 = +prompt("36/3 = ?")
+console.log("2-misol: 36/3 = " + 36/3 + " Sizning javobingiz " + task2);
+var task3 = +prompt("12*5 = ?")
+console.log("3-misol: 12*5 = " + 12*5 + " Sizning javobingiz " + task3);
+var task4 = +prompt("17%7 = ?")
+console.log("4-misol: 17%7 = " + 17%7 + " Sizning javobingiz " + task4);
+var task5 = +prompt("26 - 18 = ?")
+console.log("5-misol: 26 - 18 = " + (26-18) + " Sizning javobingiz " + task5);
+var task6 = +prompt("15 + 13 = ?")
+console.log("6-misol: 15 + 13 = " + (15+13) + " Sizning javobingiz " + task6);
+var task7 = +prompt("16*3-24/2=?")
+console.log("7-misol: 16*3-24/2 = " + (16*3-24/2) + " Sizning javobingiz " + task7);
+var task8 = +prompt("12-4+6/3=?")
+console.log("8-misol: 12-4+6/3 = " + (12-4+6/3) + " Sizning javobingiz " + task8);
+var task9 = +prompt("26%3 = ?")
+console.log("9-misol: 26%3 = " + 26%3 + " Sizning javobingiz " + task9);
+var task10 = +prompt("3*4/2-5+7=?")
+console.log("10-misol: 3*4/2-5+7 = " + (3*4/2-5+7) + " Sizning javobingiz " + task10);
+var task11 = +prompt("3*3-18/3+2*2=?")
+console.log("11-misol: 3*3-18/3+2*2= " + (3*3-18/3+2*2) + " Sizning javobingiz " + task11);
+var task12 = +prompt("16-4+9-17=?")
+console.log("12-misol: 16-4+9-17= " + (16-4+9-17) + " Sizning javobingiz " + task12);
+var task13 = +prompt("32%7 = ?")
+console.log("13-misol: 32%7 = " + 32%7 + " Sizning javobingiz " + task13);
+var task14 = +prompt("15*3/5-7+3=?")
+console.log("14-misol: 15*3/5-7+3= " + (15*3/5-7+3) + " Sizning javobingiz " + task14);
+var task15 = +prompt("7-4*3=?")
+console.log("15-misol: 7-4*3= " + (7-4*3) + " Sizning javobingiz " + task15);
+var task16 = +prompt("73%18=?")
+console.log("16-misol: 73%18= " + 73%18 + " Sizning javobingiz " + task16);
+var task17 = +prompt("7*8/2/4*3=?")
+console.log("17-misol: 7*8/2/4*3= " + (7*8/2/4*3) + " Sizning javobingiz " + task17);
+var task18 = +prompt("2*6+7+3-5=?")
+console.log("18-misol: 2*6+7+3-5= " + (2*6+7+3-5) + " Sizning javobingiz " + task18);
+var task19 = +prompt("14%5=?")
+console.log("19-misol: 14%5= " + 14%5 + " Sizning javobingiz " + task19);
+var task20 = +prompt("4*6/12*18=?")
+console.log("20-misol: 4*6/12*18= " + 4*6/12*18 + " Sizning javobingiz " + task20);
+var result = alert("Javobingizni tekshirish uchun consolega kiring")
